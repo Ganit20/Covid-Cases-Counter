8366aea5952a93c29ca9341a40b985a0acd912d0
Bartlomiej Sulek <bartlomiej.sulek@idealogic.pl>
2020-11-24 20:46:07 +0100

    pasing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+      <div id="main">
+         <div></div>
+      </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const axios = require("axios");
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      data: []
+    };
+  },
+  beforeMount(){
+    this.getName();
+  },
+  methods: {
+    async getName(){
+      const { data } = await axios.get("https://covid19-api.org/api/status");
+      for(var i in data)
+      this.data.push( data [i]);
+      
+      this.data.forEach(element=> {
+          document.getElementById("main").innerHTML +="Country: " + element.country + "<BR>";
+          console.log(element);
+      });
+   
+    }
+   
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
